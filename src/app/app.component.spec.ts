1ff230ce1750d49903d0cac73be63d48549b12f0
Hema Reddy <hemareddyr05@gmail.com>
2022-06-21 19:47:15 +0530

    commited

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -16,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'hemaKitchen_website_ecommerce'`, () => {
+  it(`should have as title 'cloudkitchenweb'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('hemaKitchen_website_ecommerce');
+    expect(app.title).toEqual('cloudkitchenweb');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('hemaKitchen_website_ecommerce app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('cloudkitchenweb app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hemaKitchen_website_ecommerce';
+  title = 'cloudkitchenweb';
 }
